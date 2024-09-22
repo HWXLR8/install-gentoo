61d975d1e2ccdb21061c1fb468d685b009e04782
HWXLR8 <68k@segv.moe>
2024-09-21 21:03:26 -0400

    add install script for non-encrypted process

---
+++ nocrypt/post-chroot.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# colors
+RED='\033[0;31m'
+NC='\033[0m'
+
+function confirm {
+    read -p "$1 [y/N] " -r
+    if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo "quitting"
+	exit
+    fi
+}
+
+function LOG {
+    echo -e "${RED}${1}${NC}"
+}
+
+### ENV INIT ###
+source /etc/profile
+
+### PORTAGE TREE ###
+LOG "SYNCING PORTAGE TREE"
+emerge-webrsync
+
+### USER ###
+LOG "SET ROOT PASSWORD"
+passwd
+LOG "CREATING UNPRIVLEGED USER"
+read -p "Enter unprivleged username: " USERNAME
+useradd -g users -G wheel,portage,audio,video,usb,cdrom -m $USERNAME
+LOG "SET UNPRIVLEGED USER PASSWORD"
+passwd $USERNAME
+
+### LOCALE ###
+LOG "SETTING LOCALE"
+echo 'LANG="en_US.UTF-8"' > /etc/env.d/02locale
+echo 'LC_COLLATE="C"' >> /etc/env.d/02locale
+echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
+echo 'C.UTF8 UTF-8' >> /etc/locale.gen
+LOG "GENERATING LOCALE"
+locale-gen
+
+### HOSTNAME ###
+LOG "SET HOST NAME"
+read -p "Enter hostname: " HOST
+echo '$HOST' > /etc/hostname
+
+### TIMEZONE ###
+LOG "SETTING TIMEZONE"
+ln -sf /usr/share/zoneinfo/Canada/Eastern /etc/localtime
+
+### KERNEL ###
+LOG "UNMASKING sys-kernel/linux-firmware"
+echo 'sys-kernel/linux-firmware linux-fw-redistributable' > /etc/portage/package.license
+LOG "EMERGE KERNEL SOURCES/FW"
+emerge -a sys-kernel/gentoo-sources sys-kernel/linux-firmware
+LOG "ESELECT KERNEL"
+eselect kernel list
+confirm "is setting kernel option to 1 ok?"
+eselect kernel set 1
+cd /usr/src/linux
+LOG "CONFIGURING KERNEL W/ localyesconfig"
+make localyesconfig
+# make -j$(nproc)
+# make modules_install
+# make install
+# confirm "continue?"
+
+### INITRAMFS ###
+LOG "INSTALLING GENKERNEL"
+emerge -a genkernel
+LOG "GENERATING INITRAMFS WITH LUKS SUPPORT"
+genkernel --lvm --luks --install all
+
+### GRUB ###
+LOG "WRITING GRUB CONFIG TO make.conf"
+echo 'GRUB_PLATFORMS="i386-pc"' >> /etc/portage/make.conf
+LOG "INSTALLING GRUB"
+emerge -a sys-boot/grub
+grub-install --target=i386-pc $ROOTD
+LOG "GENERATING GRUB CFG"
+grub-mkconfig -o /boot/grub/grub.cfg
+LOG "INSTALLING DHCP CLIENT"
+emerge -a net-misc/dhcpcd
+LOG "DONE"
---
+++ nocrypt/pre-chroot.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+ROOTD=/dev/sda
+
+# copy the above variables into post-chroot.sh
+sed -i '3i\ROOTD='"$ROOTD"'\n' post-chroot.sh
+
+# colors
+RED='\033[0;31m'
+NC='\033[0m'
+
+function confirm {
+    read -p "$1 [y/N] " -r
+    if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+	echo "quitting"
+	exit
+    fi
+}
+
+function LOG {
+    echo -e "${RED}${1}${NC}"
+}
+
+# check if user is root
+if [ "$EUID" -ne 0 ]
+then echo "please run as root"
+     exit
+fi
+
+### install prerequisites ###
+pacman -Sy wget
+
+### format disk ###
+lsblk
+echo
+echo -n -e "Gentoo will be installed onto the following disks:\n\n \
+	$ROOTD	/ \n\n"
+confirm "continue?"
+
+### mkfs ###
+LOG "CREATING EXT4 FS ROOT DEVICE"
+mkfs.ext4 $ROOTD
+lsblk -f
+confirm "continue?"
+
+### download and verify stage3 tarball ###
+TARBALL_ROOT_URL="https://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-openrc/"
+# curl and grep for latest tarball name
+TARBALL_NAME=$(curl -s $TARBALL_ROOT_URL/latest-stage3-amd64-openrc.txt | grep stage3 | awk '{print $1}')
+TARBALL_URL=$TARBALL_ROOT_URL/$TARBALL_NAME
+DIGESTS_URL=$TARBALL_URL.DIGESTS
+SIG_URL=$TARBALL_URL.asc
+mkdir gentoo
+LOG "MOUNTING ROOT"
+mount $ROOTD gentoo
+lsblk -f
+confirm "continue?"
+cd gentoo
+LOG "DOWNLOADING TARBALL"
+wget $TARBALL_URL
+wget $DIGESTS_URL
+wget $SIG_URL
+LOG "PRESENT WORKING DIR"
+pwd
+LOG "DIR CONTENTS"
+ls -lh
+confirm "continue?"
+TARBALL=${TARBALL_URL##*/}
+echo
+LOG "********** DIGESTS FILE **********"
+cat $TARBALL.DIGESTS
+echo
+LOG "********** COMPUTED DIGESTS **********"
+sha512sum $TARBALL
+echo
+confirm "Do the above digests match up?"
+# fetch all gentoo release keys
+wget -O - https://qa-reports.gentoo.org/output/service-keys.gpg | gpg --import
+echo
+gpg --verify $TARBALL.asc
+echo
+echo -e "the gentoo release fingerprint is ${RED}13EBBDBEDE7A12775DFDB1BABB572E0E2D182910${NC}"
+echo
+confirm "Do the above fingerprints match up?"
+echo
+echo "everything checks out, proceed."
+tar xpvf $TARBALL
+
+### chroot ###
+LOG "COPYING post-chroot.sh INTO CHROOT"
+cp -v ../post-chroot.sh .
+LOG "BEGIN CHROOT"
+mount -t proc none proc
+mount --rbind /sys sys
+mount --make-rslave sys
+mount --rbind /dev dev
+mount --make-rslave dev
+cp /etc/resolv.conf etc
+chroot . /bin/bash
