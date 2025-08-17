191e2180f596b6357b8c6f6d66a42dad9e90b372
HWXLR8 <68k@segv.moe>
2025-08-17 17:01:52 -0400

    [rpi5] add working firmware seq

--- rpi5/post-chroot.sh
+++ rpi5/post-chroot.sh
@@ -8,10 +8,3 @@ DISK=/dev/sda
 
 log "syncing portage tree"
 emerge-webrsync
-log "emerge kernel sources and firmware"
-echo "sys-boot/raspberrypi-firmware raspberrypi-videocore-bin" >> /etc/portage/package.license
-echo "sys-kernel/raspberrypi-image raspberrypi-videocore-bin" >> /etc/portage/package.license
-emerge -a \
-       sys-kernel/raspberrypi-sources \
-       sys-boot/raspberrypi-firmware \
-       sys-kernel/raspberrypi-image
--- rpi5/pre-chroot.sh
+++ rpi5/pre-chroot.sh
@@ -8,6 +8,18 @@ WORK=/tmp/gentoo
 DISK=/dev/sda
 GENTOO=/mnt/gentoo
 
+CONFIG=$(cat <<'EOF'
+# have a properly sized image
+disable_overscan=1
+
+# Enable audio (loads snd_bcm2835)
+dtparam=audio=on
+
+# Enable DRM VC4 V3D driver
+dtoverlay=vc4-kms-v3d-pi5
+EOF
+)
+
 function prepare_disk {
     log "partitioning $DISK"
     parted -s $DISK mklabel msdos \
@@ -28,7 +40,7 @@ function prepare_disk {
     mount ${DISK}2 $GENTOO
 }
 
-function download_stage3 {
+function install_stage3 {
     ROOT_URL="https://distfiles.gentoo.org/releases/arm64/autobuilds/current-stage3-arm64-openrc/"
     NAME=$(curl -s ${ROOT_URL}latest-stage3-arm64-openrc.txt | grep stage3 | awk '{print $1}')
     URL=${ROOT_URL}${NAME}
@@ -67,13 +79,62 @@ function download_gentoo_snapshot {
     tar xpvf "$SNAPSHOT" --strip-components=1 -C ${GENTOO}/var/db/repos/gentoo
 }
 
-function gentoo-chroot {
-    cd $GENTOO
+function install_firmware {
     log "mounting /boot"
-    mount ${DISK}1 boot
+    mount ${DISK}1 $GENTOO/boot
+    cd $WORK
+
+    git clone --depth=1 https://github.com/raspberrypi/firmware.git
+    cp -v firmware/boot/bcm2712-rpi-5-b.dtb ${GENTOO}/boot/
+    cp -v firmware/boot/fixup_cd.dat ${GENTOO}/boot/
+    cp -v firmware/boot/fixup.dat ${GENTOO}/boot/
+    cp -v firmware/boot/start_cd.elf ${GENTOO}/boot/
+    cp -v firmware/boot/start.elf ${GENTOO}/boot/
+    cp -v firmware/boot/bootcode.bin ${GENTOO}/boot/
+    cp -v firmware/boot/kernel8.img ${GENTOO}/boot/
+    cp -rv firmware/boot/overlays ${GENTOO}/boot/
+
+    log "writing cmdline.txt"
+    echo "dwc_otg.lpm_enable=0 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait net.ifnames=0 logo.nologo usbhid.mousepoll=1" >> ${GENTOO}/boot/cmdline.txt
+
+    log "writing config.txt"
+    echo "$CONFIG" | tee -a ${GENTOO}/boot/config.txt
+
+    log "copying firmware"
+    cp -rv firmware/modules ${GENTOO}/lib/
+
+    # wifi firmware
+    log "installing wifi fw"
+    git clone --depth=1 https://github.com/RPi-Distro/firmware-nonfree.git
+    mkdir -pv ${GENTOO}/lib/firmware/brcm
+    cp -v firmware-nonfree/debian/config/brcm80211/cypress/cyfmac43455-sdio-standard.bin ${GENTOO}/lib/firmware/brcm/
+    cp -v firmware-nonfree/debian/config/brcm80211/cypress/cyfmac43455-sdio.clm_blob ${GENTOO}/lib/firmware/brcm/
+    cp -v firmware-nonfree/debian/config/brcm80211/brcm/brcmfmac43455-sdio.txt ${GENTOO}/lib/firmware/brcm/
+    log "symlinking firmware files"
+    cd ${GENTOO}/lib/firmware/brcm/
+    ln -vs cyfmac43455-sdio-standard.bin brcmfmac43455-sdio.raspberrypi,5-model-b.bin
+    ln -vs cyfmac43455-sdio.clm_blob brcmfmac43455-sdio.raspberrypi,5-model-b.clm_blob
+    ln -vs brcmfmac43455-sdio.txt brcmfmac43455-sdio.raspberrypi,5-model-b.txt
+    cd $WORK
+    ls -l ${GENTOO}/lib/firmware/brcm/
+    confirm "symlinks look good?"
+
+    # bluetooth
+    log "installing bluetooth firmware"
+    git clone --depth=1 https://github.com/RPi-Distro/bluez-firmware.git
+    mkdir -pv ${GENTOO}/lib/firmware/brcm
+    cp bluez-firmware/debian/firmware/broadcom/BCM4345C0.hcd ${GENTOO}/lib/firmware/brcm/
+    log "symlinking firmware files"
+    cd ${GENTOO}/lib/firmware/brcm/
+    ln -sv BCM4345C0.hcd BCM4345C0.raspberrypi,5-model-b.hcd
+    cd $WORK
+}
+
+function gentoo-chroot {
     log "copying post-chroot.sh into chroot"
-    cp -v ../post-chroot.sh .
+    cp -v post-chroot.sh $GENTOO
     log "begin chroot"
+    cd $GENTOO
     mount -t proc none proc
     mount --rbind /sys sys
     mount --make-rslave sys
@@ -83,7 +144,7 @@ function gentoo-chroot {
     chroot . /bin/bash
 }
 
-prepare_disk
-download_stage3
-### download_gentoo_snapshot
+#prepare_disk
+#install_stage3
+#install_firmware
 gentoo-chroot
