7ceade83e1a235545f280ed461f2f3fabb1c7d1a
HWXLR8 <68k@segv.moe>
2025-08-27 00:36:14 -0400

    [rpi5] emerge sudo and edit sudoers

---
+++ rpi5/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function log {
+    RED='\033[0;31m'
+    NC='\033[0m'
+    echo -e "${RED}* ${1}${NC}"
+}
+
+log "emerging sudo"
+USE="-sendmail" emerge sudo
+log "modifying /etc/sudoers"
+sed -i '/%wheel ALL=(ALL:ALL) ALL/ s/^[[:space:]]*#[[:space:]]*//' /etc/sudoers
