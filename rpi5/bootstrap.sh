004f32a7cb75a353aeb81fa00538cabfffa5f382
HWXLR8 <68k@segv.moe>
2025-08-31 13:02:52 -0400

    [rpi5] move make.conf confit to post-chroot

--- rpi5/bootstrap.sh
+++ rpi5/bootstrap.sh
@@ -13,11 +13,3 @@ log "emerging sudo"
 USE="-sendmail" emerge sudo
 log "modifying /etc/sudoers"
 sed -i '/%wheel ALL=(ALL:ALL) ALL/ s/^[[:space:]]*#[[:space:]]*//' /etc/sudoers
-
-### make.conf ###
-log "adding USE to make.conf"
-echo 'USE="-systemd -selinux elogind"' >> /etc/portage/make.conf
-log "adding ACCEPT_KEYWORDS to make.conf"
-echo 'ACCEPT_KEYWORDS="~arm64 arm64"' >> /etc/portage/make.conf
-log "adding VIDEO_CARDS to make.conf"
-echo 'VIDEO_CARDS="v3d"' >> /etc/portage/make.conf
--- rpi5/post-chroot.sh
+++ rpi5/post-chroot.sh
@@ -39,6 +39,14 @@ ln -sf /usr/share/zoneinfo/Canada/Eastern /etc/localtime
 log "creating tmpfs for /tmp"
 echo "tmpfs                    /tmp           tmpfs    defaults,noatime,nosuid,nodev,mode=1777,size=10240M 0 0" >> /etc/fstab
 
+### PORTAGE ###
+log "adding USE to make.conf"
+echo 'USE="-systemd -selinux elogind"' >> /etc/portage/make.conf
+log "adding ACCEPT_KEYWORDS to make.conf"
+echo 'ACCEPT_KEYWORDS="~arm64 arm64"' >> /etc/portage/make.conf
+log "adding VIDEO_CARDS to make.conf"
+echo 'VIDEO_CARDS="v3d"' >> /etc/portage/make.conf
+
 ### MISC ###
 # this stops the following error from spamming the term:
 # INIT Id "f0" respawning too fast: disabled for 5 minutes
