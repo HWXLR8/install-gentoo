edc360af0aa4cb79509545fbf1968b7b471cfb8d
HWXLR8 <68k@segv.moe>
2025-08-31 12:59:33 -0400

    [rpi5] add make.conf vars

--- rpi5/bootstrap.sh
+++ rpi5/bootstrap.sh
@@ -8,7 +8,16 @@ function log {
     echo -e "${RED}* ${1}${NC}"
 }
 
+### sudo ###
 log "emerging sudo"
 USE="-sendmail" emerge sudo
 log "modifying /etc/sudoers"
 sed -i '/%wheel ALL=(ALL:ALL) ALL/ s/^[[:space:]]*#[[:space:]]*//' /etc/sudoers
+
+### make.conf ###
+log "adding USE to make.conf"
+echo 'USE="-systemd -selinux elogind"' >> /etc/portage/make.conf
+log "adding ACCEPT_KEYWORDS to make.conf"
+echo 'ACCEPT_KEYWORDS="~arm64 arm64"' >> /etc/portage/make.conf
+log "adding VIDEO_CARDS to make.conf"
+echo 'VIDEO_CARDS="v3d"' >> /etc/portage/make.conf
